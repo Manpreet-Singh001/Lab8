0a628a88a5ed7f93dbb3ef1e96b0ccea44c8acde
Manpreet <manpreetrahal27@gmail.com>
2022-10-31 01:58:00 -0600

    Added: Test to add a new city to CustomList

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,6 +11,26 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
+
+    /**
+     * create a mock CustomList
+     *
+     * @return list
+     */
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
 
 
+    /**
+     * add content to our list
+     * @return Null
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        list.add(new City("Edmonton", "AB"));
+    }
 }
